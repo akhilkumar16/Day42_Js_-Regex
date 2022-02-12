8f66b073aa9790517e664744da225529260db19b
Your Name <akhilkumarchandragiri@gmail.com>
2022-02-12 16:25:37 +0530

    [Akhil] pincode uc3

--- Regex.js
+++ Regex.js
@@ -34,3 +34,20 @@
     }
     validatePINCode();
 }
+//UC3//
+{
+    let validatePINCode = () => {
+        console.log("\n pincode given");
+        try {
+            let pincode = "400088B"//"Enter pincode:";
+            let pincodePattern = new RegExp('^[1-9]{1}[0-9]{5,}$');
+            if (pincodePattern.test(pincode))
+                console.log('Pincode is valid');
+            else
+                throw 'Error! Pincode is not valid';
+        }
+        catch (e) {
+            console.error(e);
+        }
+    }
+    validatePINCode();}
